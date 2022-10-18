e8fcdfafafd7bfab7857abda49e813f6b27986a0
M Siddiq Bayu Seto <91077266+SiddiqBayu23@users.noreply.github.com>
2022-10-18 22:39:48 +0700

    Add files via upload

---
+++ Pertemuan 5/Script/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    float hinput;
+    float vinput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hinput = Input.GetAxis("Horizontal");
+        vinput = Input.GetAxis("Vertical");
+
+       if(Input.GetKey(KeyCode.Space)){
+
+       
+       transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+       transform.Rotate(0,hinput * 100 * Time.deltaTime, 0);
+       }
+    }
+}
